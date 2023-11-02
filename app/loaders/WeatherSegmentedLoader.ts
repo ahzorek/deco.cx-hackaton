12aa3936eff6f50ab0f6440c523252c77e78ea2a
Andre <andrezorek@gmail.com>
2023-11-02 01:31:49 -0300

    a a better loader. no api keys were leaked here. ok maybe one

--- app/loaders/WeatherSegmentedLoader.ts
+++ app/loaders/WeatherSegmentedLoader.ts
@@ -6,10 +6,27 @@ export interface ItemsQuantity {count: number};
 
 export default async function WeatherSegmentedLoader({ count }: ItemsQuantity, req: Request, ctx: AppContext,): Promise<Product[] | null> {
   
-  const temp = await weather({ lat: '25.2048', long: '55.2708' })
+  const ipResponse = await fetch('https://api.ipgeolocation.io/getip');
+  const {ip} = await ipResponse.json();
+  // const userIp = ipData.ip;
   
-  console.log('REQUEST::', req);
-  console.log('AppContext::', ctx);
+  //  Use o endereço IP do usuário para obter a localização
+  const response = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=cabd772e8c8d44148c531c2037fdf960&ip=${ip}`);
+  const locationData = await response.json();
+  console.log(locationData);
+  
+  const {latitude, longitude} = locationData
+  
+  const temp = await weather({ lat: latitude, long: longitude })
+
+  console.log('LATITUDE:::', latitude, 'LONGITUDE:::', longitude);
+  console.log('TEMPERATURA ::::', temp);
+  
+  
+  
+  // console.log('LAT::', req.get("cf-iplatitude"))
+
+  // console.log('AppContext::', ctx)
   
   const clothesLoading = temp.celsius > 24 ? 'camiseta' : 'casaco'
 
